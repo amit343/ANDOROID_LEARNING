0bad20738cf129ec6fdd276c643c7d546a834998
REJECTION-POINT <amitparajapat343@gmail.com>
2019-06-24 10:13:17 -0700

    Create call demo application

--- createcall/src/main/java/com/example/createcall/MainActivity.java
+++ createcall/src/main/java/com/example/createcall/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.createcall;
 
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int REQUEST_PHONE_CALL = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
     }
+
+    public void workwithintent1(View view){
+
+
+        /*
+        Intent intent =  new Intent();
+        intent.setAction("com.codekul.action.news");
+        intent.setData( Uri.parse( "tel://123213213123" ));
+        startActivity(intent);
+
+        */
+        call();
+    }
+    private void call() {
+        final  EditText phone  = (EditText) findViewById( R.id.phone );
+        String content = phone.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + content));
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE},REQUEST_PHONE_CALL);
+            }
+            else
+            {
+                startActivity(intent);
+            }
+        }
+        else
+        {
+            startActivity(intent);
+        }
+    }
+    private void dial(){
+
+        Intent intent = new Intent( Intent.ACTION_DIAL );
+        startActivity( intent );
+    }
 }
+
